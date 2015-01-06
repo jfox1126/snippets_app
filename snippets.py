82a48bbd4e9091f80c9653bed1d1397d69e40afc
Jared Jones <jfox1126@gmail.com>
2015-01-06 19:50:17 +0000

    put command works

--- snippets.py
+++ snippets.py
@@ -1,17 +1,23 @@
+import psycopg2
 import logging
 import argparse
 import sys
+
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect("dbname='snippets' user='action' host='localhost'")
+logging.debug("Database connection established.")
 
 #Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 def put(name, snippet):
-  """
-  Store a snippet with an associated name.
-
-  Returns the name and the snippet
-  """
+  """Store a snippet with an associated name."""
   logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+  cursor = connection.cursor()
+  command = "insert into snippets values (%s, %s)"
+  cursor.execute(command, (name, snippet))
+  connection.commit()
+  logging.debug("Snippet stored successfully.")
   return name, snippet
 
 def get(name):
@@ -57,7 +63,22 @@ def main():
   put_parser.add_argument("name", help="The name of the snippet")
   put_parser.add_argument("snippet", help="The snippet text")
   
+  # Subparser for the get command
+  logging.debug("Constructing get subparser")
+  get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+  get_parser.add_argument("name", help="The name of the snippet to retrieve")
+  
   arguments = parser.parse_args(sys.argv[1:])
+  # Convert parsed arguments from Namespace to dictionary
+  arguments = vars(arguments)
+  command = arguments.pop("command")
+  
+  if command == "put":
+    name, snippet = put(**arguments)
+    print("Stored {!r} as {!r}".format(snippet, name))
+  elif command == "get":
+    snippet = get(**arguments)
+    print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__=="__main__":
   main()
