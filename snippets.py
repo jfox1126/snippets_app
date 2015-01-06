72e0326a5f6d0b96916368293e34d2f80682a02c
Jared Jones <jfox1126@gmail.com>
2015-01-06 01:39:53 +0000

    basic parsing

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+import sys
 
 #Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -39,3 +41,23 @@ def delete(name):
   """
   logging.error("FIXME: Unimplemented - delete({!r})".format(name))
   return ""
+
+
+
+def main():
+  """Main function"""
+  logging.info("Constructing parser")
+  parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+  
+  subparsers = parser.add_subparsers(dest="command", help="Available commands")
+  
+  # Subparser for the put command
+  logging.debug("Constructing put subparser")
+  put_parser = subparsers.add_parser("put", help="Store a snippet")
+  put_parser.add_argument("name", help="The name of the snippet")
+  put_parser.add_argument("snippet", help="The snippet text")
+  
+  arguments = parser.parse_args(sys.argv[1:])
+
+if __name__=="__main__":
+  main()
